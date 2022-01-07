6f646037c1aafed2001147e3ca5d5e3cbd51bdc5
Joao <joaovictorcgjvcg@gmail.com>
2022-01-07 14:38:20 -0400

    Entrega do requisito 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,59 @@
 import React from 'react';
 
 class Form extends React.Component {
-
   render() {
     return (
       <form>
-        <input data-testid="name-input" type='text'></input>
-        <textarea data-testid="description-input"></textarea>
-        <input data-testid="attr1-input" type='number'></input>
-        <input data-testid="attr2-input" type='number'></input>
-        <input data-testid="attr3-input" type='number'></input>
-        <input data-testid="image-input" type='text'></input>
+        <input
+          type="text"
+          data-testid="name-input"
+        />
+
+        <textarea
+          data-testid="description-input"
+        />
+
+        <input
+          type="number"
+          data-testid="attr1-input"
+        />
+
+        <input
+          type="number"
+          data-testid="attr2-input"
+        />
+
+        <input
+          type="number"
+          data-testid="attr3-input"
+        />
+
+        <input
+          type="text"
+          data-testid="image-input"
+        />
+
         <select data-testid="rare-input">
-          <option>normal</option>
-          <option>raro</option>
-          <option>muito raro</option>
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
         </select>
-        <input data-testid="trunfo-input" type='checkbox'></input>
-        <button data-testid="save-button">Salvar</button>
+
+        <input
+          type="checkbox"
+          data-testid="trunfo-input"
+        />
+
+        <button
+          type="button"
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
+
       </form>
-    )
+    );
   }
 }
 
-export default Form
+export default Form;
